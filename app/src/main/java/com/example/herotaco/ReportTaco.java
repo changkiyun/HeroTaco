f8ffaaf972bd28759d0dd21af55e8ef1d8bae044
changkiyun <ekzm7845@naver.com>
2023-12-04 10:40:00 +0900

    reportTaco naverMapConnection

---
+++ app/src/main/java/com/example/herotaco/ReportTaco.java
@@ -0,0 +1,14 @@
+package com.example.herotaco;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReportTaco extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report_taco);
+    }
+}
